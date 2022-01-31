f91e6b6e8eb83f3b96fa821ac4072aab26922105
John DeWyze <john@dewyze.dev>
2022-01-31 12:39:51 -0600

    Add dotenv and set vim executable and return function
    This adds the 'dotenv' gem to require setting a vim executable path.
    Also, when using MacVim, MacVim steals focus during tests and then does
    not return it.
    
    This allows someone to set a "return command" that will be run from vim
    to go back to the calling application. For example:
    `open /Applications/Alacritty.app`.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'http://rubygems.org'
 
+gem "dotenv"
 gem "pry-byebug"
 gem "rspec"
 gem "vimrunner"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,20 @@
+require "dotenv/load"
 require "pry"
 require "tempfile"
 require "vimrunner"
 require "vimrunner/rspec"
 
-VIM_PATH = "/Applications/MacVim.app/Contents/bin/mvim"
+class VimExecutablePathMissing < StandardError
+  def message
+    "You need to set the VIM_TADA_VIM_PATH environment variable in the .env file with the path to your vim executable."
+  end
+end
 
 Vimrunner::RSpec.configure do |config|
   config.reuse_server = true
+  raise VimExecutablePathMissing unless ENV["VIM_TADA_VIM_PATH"]
   config.start_vim do
-    vim = Vimrunner::Server.new(executable: VIM_PATH).start
+    vim = Vimrunner::Server.new(executable: ENV["VIM_TADA_VIM_PATH"]).start
     vim.prepend_runtimepath(File.expand_path('../..', __FILE__))
     vim.command "runtime ftdetect/tada.vim"
     vim.add_plugin(File.expand_path('../vim', __FILE__), 'plugin/syntax_test.vim')
@@ -28,5 +34,11 @@ Vimrunner::RSpec.configure do |config|
         expect(vim.echo("TestSyntax('#{pattern}', '#{group}')")).to eq '1'
       end
     end
+  end
+end
+
+RSpec.configure do |config|
+  config.after(:all) do
+    vim.command "!#{ENV["VIM_TADA_REFOCUS_COMMAND"]}" if ENV["VIM_TADA_REFOCUS_COMMAND"]
   end
 end
