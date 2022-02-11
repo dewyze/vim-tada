61f9e22a50da7285875886ffd5d6323b2341396f
John DeWyze <john@dewyze.dev>
2022-02-10 21:15:44 -0600

    Round out box toggle features
    This covers box toggle in a few more situations and fixes a few bugs

--- autoload/tada/map.vim
+++ autoload/tada/map.vim
@@ -16,15 +16,31 @@ function! tada#map#ToggleBox()
   let save_col = col('.')
   let boxlen = len('[' . tada#todo#DefaultSymbol() . '] ')
   let spaces = indent('.')
+  let blank_box = '[' . tada#todo#DefaultSymbol() . '] '
 
-  if text =~ '^\s*-\?\s*$'
+  " if line above is todo, put it at the same level
+  if text =~ '^\s*$'
+    let prev_line_num = line('.') - 1
+    if tada#SyntaxGroupOfLine(prev_line_num) =~ '^tadaTodoItem'
+      let spaces = repeat(' ', max([indent(prev_line_num), 0]))
+      call setline('.', spaces . '- ' . blank_box)
+      call cursor(line('.'), s:NewColPos(save_col, 0))
+    else
+      call setline('.', repeat(' ', spaces) . '- ' . blank_box)
+      normal! $
+    endif
+  elseif text =~ '^\(\s*-\)\@!.\+$'
+    let spaces = repeat(' ', max([indent('.') - 6, 0]))
+    call setline('.', substitute(getline('.'), '^\(\s*\)\(\S.*\)$', spaces . '- ' . blank_box . '\2', ''))
+    call cursor(line('.'), s:NewColPos(save_col, 0))
+  elseif text =~ '^\s*-\s*$'
     call setline('.', substitute(getline('.'), '^\(\s*\).*$', '\1- [ ] ', ''))
     normal! $
   elseif group =~ '^tadaTodoItem'
     call setline('.', substitute(getline('.'), '^\(\s*\)-\s*\[.\]\s*\(.*\)$', '\1- \2', ''))
     call cursor(line('.'), s:NewColPos(save_col, boxlen))
   elseif group == 'tadaListItem' || group =~ '^tadaTopicTitle'
-    call setline('.', substitute(getline('.'), '^\(\s*-\s*\)\(\S.*\)$', '\1[' . tada#todo#DefaultSymbol() . '] \2', ''))
+    call setline('.', substitute(getline('.'), '^\(\s*-\s*\)\(\S.*\)$', '\1' . blank_box . '\2', ''))
     call cursor(line('.'), s:NewColPos(save_col, -1 * boxlen))
   endif
 endfunction
@@ -32,20 +48,11 @@ endfunction
 function! s:NewColPos(colpos, offset)
   let spaces = indent('.')
 
-  if a:colpos > spaces + len('- ') + a:offset
+  if a:colpos <= spaces + len('- ')
+    return a:colpos
+  elseif a:colpos > spaces + len('- ') + a:offset
     return a:colpos - a:offset
   else
     return spaces + len('- ') + 1
   endif
 endfunction
-  "
-  " if text =~ '^\s*-\?\s*$'
-  "   call setline('.', substitute(getline('.'), '^\(\s*\).*$', '\1- [ ] ', ''))
-  "   normal! $
-  " elseif group =~ '^tadaTodoItem'
-  "   call setline('.', substitute(getline('.'), '^\(\s*-\)\s*\[.\]\s*\(.*\)$', '\1 \2', ''))
-  "   call cursor(line('.'), s:NewColPos(save_col, boxlen))
-  " elseif group == 'tadaListItem' || group =~ '^tadaTopicTitle'
-  "   call setline('.', substitute(getline('.'), '^\(\s*-\s*\)\(\S.*\)$', '\1[' . tada#todo#DefaultSymbol() . '] \2', ''))
-  "   call cursor(line('.'), s:NewColPos(save_col, -1 * boxlen))
-  " endif
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "map specs" do
-  describe "<C-B> adding/removing boxes" do
+  describe "<C-B> converting boxes to list items" do
     context "in normal mode" do
       it "doesn't move the cursor if after the box" do
         content = <<~CONTENT
@@ -68,13 +68,6 @@ RSpec.describe "map specs" do
           vim.feedkeys '\<C-B>aHello'
           vim.write
 
-          # This one is weird. I wish it would produce the empty space between
-          # them, but it does not in normal mode. It's also made complicated
-          # by the fact that I trim all whitespace at the end of lines (thus
-          # all the "Hello" strings added so they work in my tests.
-          # For now, I'm not worried about someone changing a box but not
-          # typing. If they have an empty string, they can still do it in one
-          # character with 'a'.
           expect(file.read).to eq(<<~NEW)
             - Hello
           NEW
@@ -136,6 +129,108 @@ RSpec.describe "map specs" do
           file.rewind
           expect(file.read).to eq(<<~NEW)
             - [ ] My Todo item
+          NEW
+        end
+      end
+
+      it "works with an empty box" do
+        content = <<~CONTENT
+          - [ ]
+        CONTENT
+
+        with_file(content) do |file|
+          vim.normal "gg$"
+          vim.feedkeys 'i\<C-B>Hello'
+          vim.write
+
+          expect(file.read).to eq(<<~NEW)
+            - Hello
+          NEW
+        end
+      end
+    end
+  end
+
+  describe "<C-B> converting list items to boxes" do
+    context "in normal mode" do
+      it "doesn't move the cursor if after the box" do
+        content = <<~CONTENT
+          - Todo item
+        CONTENT
+
+        with_file(content) do |file|
+          vim.normal "gg02w"
+          vim.feedkeys '\<C-B>'
+          vim.write
+
+          expect(file.read).to eq(<<~NEW)
+            - [ ] Todo item
+          NEW
+        end
+      end
+
+      it "doesn't move the cursor if before the box" do
+        content = <<~CONTENT
+          - Todo item
+        CONTENT
+
+        with_file(content) do |file|
+          vim.normal "gg0"
+          vim.feedkeys '\<C-B>'
+          vim.write
+
+          expect(file.read).to eq(<<~NEW)
+            - [ ] Todo item
+          NEW
+        end
+      end
+
+      it "works with an empty item" do
+        content = <<~CONTENT
+          -
+        CONTENT
+
+        with_file(content) do |file|
+          vim.normal "gg$"
+          vim.feedkeys '\<C-B>AHello'
+          vim.write
+
+          expect(file.read).to eq(<<~NEW)
+            - [ ] Hello
+          NEW
+        end
+      end
+    end
+
+    context "in insert mode" do
+      it "doesn't move the cursor if after the box" do
+        content = <<~CONTENT
+          - Todo item
+        CONTENT
+
+        with_file(content) do |file|
+          vim.normal "gg02wi"
+          vim.feedkeys '\<C-B>my '
+          vim.write
+
+          expect(file.read).to eq(<<~NEW)
+            - [ ] Todo my item
+          NEW
+        end
+      end
+
+      it "doesn't move the cursor if before the box" do
+        content = <<~CONTENT
+          - Todo item
+        CONTENT
+
+        with_file(content) do |file|
+          vim.normal "gg0i"
+          vim.feedkeys '\<C-B>My '
+          vim.write
+
+          expect(file.read).to eq(<<~NEW)
+            My - [ ] Todo item
           NEW
         end
       end
